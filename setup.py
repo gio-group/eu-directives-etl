57c28c9eb8fd224c2cbc9b09791f55b029f04d8a
Victor Costa <victorcrcosta@gmail.com>
2023-10-02 23:59:00 +0100

    Added 'setup.py' and rearranged project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="eu-directives-etl",
+    version="1.0.0",
+    description="ETL script for EU's directives",
+    license="MIT",
+    author="Victor Costa",
+    author_email="victorcrcosta@gmail.com",
+    url="https://github.com/gio-group/eu-directives-etl",
+    packages=["src"],
+)
