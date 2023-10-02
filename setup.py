4a9ead41f1d5595106ca7e71c9a1357ca391a60d
Victor Costa <victorcrcosta@gmail.com>
2023-10-03 00:16:59 +0100

    Updated project's structure to work as a package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name="eu_directives_etl",
+    name="eu-directives-etl",
     version="1.0.0",
     description="ETL script for EU's directives",
     license="MIT",
     author="Victor Costa",
     author_email="victorcrcosta@gmail.com",
     url="https://github.com/gio-group/eu-directives-etl",
-    packages=["src"],
+    packages=["directives_etl"],
 )
